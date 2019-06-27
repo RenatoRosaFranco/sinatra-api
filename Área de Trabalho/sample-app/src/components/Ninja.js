fd879ed6d9ed4cde5279314d5fc88afb2afb2fb8
Renato Rosa Franco <renato_ny@live.com>
2019-06-27 16:58:10 -0300

    Conditional statement

--- Área de Trabalho/sample-app/src/components/Ninja.js
+++ Área de Trabalho/sample-app/src/components/Ninja.js
@@ -5,11 +5,15 @@ class Ninjas extends Component {
     render() {
         const ninjas = this.props.ninjas;
         const ninjaList = ninjas.map((ninja) => {
+            if (ninja.age > 20 ) {
             return <div key={ninja.id} className={'ninja'}>
                         <div>Name: { ninja.name } </div>
                         <div>Age: { ninja.age} </div>
                         <div>Belt: { ninja.belt } </div>
                     </div>
+                    }else {
+                        return null;
+                    }
         })
 
         return(
