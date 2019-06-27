ddb599fa9daef4939748a00a6854c04c5c7d2bf5
Renato Rosa Franco <renato_ny@live.com>
2019-06-27 16:49:15 -0300

    Statless components

--- Área de Trabalho/sample-app/src/components/Ninja.js
+++ Área de Trabalho/sample-app/src/components/Ninja.js
@@ -4,14 +4,18 @@ class Ninjas extends Component {
 
     render() {
         const ninjas = this.props.ninjas;
+        const ninjaList = ninjas.map((ninja) => {
+            return <div key={ninja.id} className={'ninja'}>
+                        <div>Name: { ninja.name } </div>
+                        <div>Age: { ninja.age} </div>
+                        <div>Belt: { ninja.belt } </div>
+                    </div>
+        })
+
         return(
-            ninjas.map((ninja) => {
-                return <div key={ninja.id} className={'Ninja'}>
-                            <div>Name: { ninja.name } </div>
-                            <div>Age: { ninja.age} </div>
-                            <div>Belt: { ninja.belt } </div>
-                        </div>
-            })
+            <div className={'ninja-list'}>
+                {ninjaList}    
+            </div>
         )
     }
 }
